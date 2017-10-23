3a5f5799cba7fb38cf7687aad2d0f7189f8df71f
林哲弘 <makelove@gmail.com>
2017-10-23 16:34:17 +0800

    commit test
    commit test

---
+++ hermit/src/main/java/com/hermit/iii/admanager/model/ADManagerVO.java
@@ -0,0 +1,70 @@
+package com.hermit.iii.admanager.model;
+
+import java.sql.Date;
+
+public class ADManagerVO {
+	private Integer adNo; //廣告編號
+	private String adImage; //廣告圖片
+	private String adLink; //廣告超連結
+	private String adMessage; //廣告訊息
+	private Date adTimeStart; //廣告起始時間
+	private Date adTimeEnd; //廣告結束時間
+	private boolean adStatus; //廣告狀態(已上架,已下架)
+	private Integer adBrowse; //廣告瀏覽次數
+	private Integer adModify; //廣告最後修改人
+	public Integer getAdNo() {
+		return adNo;
+	}
+	public void setAdNo(Integer adNo) {
+		this.adNo = adNo;
+	}
+	public String getAdImage() {
+		return adImage;
+	}
+	public void setAdImage(String adImage) {
+		this.adImage = adImage;
+	}
+	public String getAdLink() {
+		return adLink;
+	}
+	public void setAdLink(String adLink) {
+		this.adLink = adLink;
+	}
+	public String getAdMessage() {
+		return adMessage;
+	}
+	public void setAdMessage(String adMessage) {
+		this.adMessage = adMessage;
+	}
+	public Date getAdTimeStart() {
+		return adTimeStart;
+	}
+	public void setAdTimeStart(Date adTimeStart) {
+		this.adTimeStart = adTimeStart;
+	}
+	public Date getAdTimeEnd() {
+		return adTimeEnd;
+	}
+	public void setAdTimeEnd(Date adTimeEnd) {
+		this.adTimeEnd = adTimeEnd;
+	}
+	public boolean getAdStatus() {
+		return adStatus;
+	}
+	public void setAdStatus(boolean adStatus) {
+		this.adStatus = adStatus;
+	}
+	public Integer getAdBrowse() {
+		return adBrowse;
+	}
+	public void setAdBrowse(Integer adBrowse) {
+		this.adBrowse = adBrowse;
+	}
+	public Integer getAdModify() {
+		return adModify;
+	}
+	public void setAdModify(Integer adModify) {
+		this.adModify = adModify;
+	}
+	
+}
--- hermit/src/main/java/com/hermit/iii/boroughs/model/BoroughsService.java
+++ hermit/src/main/java/com/hermit/iii/boroughs/model/BoroughsService.java
@@ -1,5 +1,7 @@
 package com.hermit.iii.boroughs.model;
 
+import java.util.List;
+
 public class BoroughsService {
 
 	private BoroughsDAO_interface dao;
@@ -8,6 +10,29 @@ public class BoroughsService {
 		dao = new BoroughsDAO_JNDI();
 	}
 	
+	public void insertBoroughs(String boroughName, Integer cityNO ){
+		BoroughsVO vo = new BoroughsVO();
+		vo.setBoroughName(boroughName);
+		vo.setCityNO(cityNO);
+		dao.insert(vo);
+	}
 	
+	public void updateBoroughs(Integer boroughNO,String boroughName,Integer cityNO){
+		BoroughsVO vo = new BoroughsVO();
+		vo.setBoroughNO(boroughNO);
+		vo.setBoroughName(boroughName);
+		vo.setCityNO(cityNO);
+		dao.update(vo);
+	}
+	
+	public void deleteBoroughs(Integer boroughNO){
+		dao.delete(boroughNO);
+	}
+	public BoroughsVO getOne(Integer boroughNO){
+		return dao.findByPrimaryKey(boroughNO);
+	}
+	public List<BoroughsVO> getAll(){
+		return dao.getAll();
+	}
 	
 }
