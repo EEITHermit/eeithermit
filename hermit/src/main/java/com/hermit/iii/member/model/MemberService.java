9d88a12b60ec0c60377f9d93338cceae6ee615f2
許竣為 <EEIT97@org>
2017-10-23 19:41:48 +0800

    10/23 fix member service init bug

--- hermit/src/main/java/com/hermit/iii/member/model/MemberService.java
+++ hermit/src/main/java/com/hermit/iii/member/model/MemberService.java
@@ -3,7 +3,7 @@ package com.hermit.iii.member.model;
 import java.util.*;
 
 public class MemberService {
-	private MemberDAO_interface dao = new MemberJNDIDAO();
+	private MemberDAO_interface dao;
 
 	public MemberService() {
 		dao = new MemberJNDIDAO();
