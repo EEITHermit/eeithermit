5e23ae82cea9314bbd02646997fe3604c0ed61fc
許竣為 <EEIT97@org>
2017-10-27 16:51:09 +0800

    10/27 hibernate 參考範例

--- hermit/src/main/java/com/hermit/iii/member/model/MemberVO_hibernate.java
+++ hermit/src/main/java/com/hermit/iii/member/model/MemberVO_hibernate.java
@@ -3,7 +3,7 @@ package com.hermit.iii.member.model;
 import java.io.Serializable;
 import java.sql.*;
 
-public class MemberVO implements Serializable {
+public class MemberVO_hibernate implements Serializable {
 	private Integer memNO;
 	private String memTel;
 	private String memAccount;
