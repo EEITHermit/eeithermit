e2d4579681605ee2834d01b6daa34349e10a849f
jlsf1 <jlsf1@DESKTOP-PHGA73T>
2017-10-28 23:47:47 +0800

    10/28 commit
    TeamArea 有問題，明天(10/29)問漢勳
    有問題部分已註解

--- hermit/src/main/java/com/hermit/iii/teamArea/model/TeamAreaDAO_interface.java
+++ hermit/src/main/java/com/hermit/iii/teamArea/model/TeamAreaDAO_interface.java
@@ -1,8 +1,11 @@
 package com.hermit.iii.teamArea.model;
 
+import java.util.List;
+
 public interface TeamAreaDAO_interface {
 	public void insert(TeamAreaVO taVO);
 	public void delete(Integer businNO);
-	public TeamAreaVO select(Integer businNO);
+	public TeamAreaVO findByPrimaryKey(Integer businNO);
 	public void update(TeamAreaVO taVO);
+	public List<TeamAreaVO>getAll();
 }
