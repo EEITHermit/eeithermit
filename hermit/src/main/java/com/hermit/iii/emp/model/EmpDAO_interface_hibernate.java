fd33144dc538dd8aef486979ed7093d7bd84f455
明凱 <sarmiley@gmail.com>
2017-11-05 11:18:21 +0800

    commit

--- hermit/src/main/java/com/hermit/iii/emp/model/EmpDAO_interface_hibernate.java
+++ hermit/src/main/java/com/hermit/iii/emp/model/EmpDAO_interface_hibernate.java
@@ -9,7 +9,6 @@ public interface EmpDAO_interface_hibernate {
 	public EmpVO findByPrimaryKey(Integer empNO);
 	public List<EmpVO> getAll();
 	public String getAllForJson();
-	public List<EmpVO> getByPost(Integer post);
 	/*** 自訂指令 ***/
 	// 驗證帳號是否存在
 	public EmpVO findByAccount(String empAccount);
